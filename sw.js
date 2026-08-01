c318d46be8eb056d2cc1c5ef302fe311d3127734
Arya <arya.sasongko@gmail.com>
2026-08-02 01:23:45 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
        offline fallback.
      - icons and the manifest are CACHE-FIRST, since they rarely change.
    Bump CACHE whenever index.html changes. */
-var CACHE = 'faham-v15';
+var CACHE = 'faham-v16';
 var ASSETS = [
   './',
   './index.html',
